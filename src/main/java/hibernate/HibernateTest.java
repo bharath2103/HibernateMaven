e35cc0adbf0f0f12dc03ca910ad595ee579be274
Bharathkumar <bharath2103@gmail.com>
2018-03-27 07:42:39 +0530

    HqlSample

--- src/main/java/hibernate/HibernateTest.java
+++ src/main/java/hibernate/HibernateTest.java
@@ -4,44 +4,29 @@ package hibernate;
 import dto.UserAddress;
 import dto.UserDetails;
 import dto.Vehicle;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
 
 import java.util.Date;
+import java.util.List;
 
 public class HibernateTest {
 
     public static void main(String[] args) {
-        UserDetails userDetails = new UserDetails();
-        userDetails.setUserName("Aishu");
-
-        Vehicle vehicleDetails1 = new Vehicle();
-        vehicleDetails1.setVehicleName("Car");
-        Vehicle vehicleDetails2 = new Vehicle();
-        vehicleDetails2.setVehicleName("Jeep");
-
-        userDetails.getVehicle().add(vehicleDetails1);
-        userDetails.getVehicle().add(vehicleDetails2);
 
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.save(userDetails);
-        session.save(vehicleDetails1);
-        session.save(vehicleDetails2);
-        session.getTransaction().commit();
-        session.close();
+        Session session1 = sessionFactory.openSession();
+        session1.beginTransaction();
+        Query query = session1.createQuery("from Vehicle");
+        List<Vehicle> listOfVehicle = (List<Vehicle>) query.list();
+        session1.getTransaction().commit();
+        session1.close();
+
+      listOfVehicle.forEach(l -> System.out.println(l.getVehicleName()));
 
 
-//        session = sessionFactory.openSession();
-//        session.beginTransaction();
-//
-//        UserDetails userDetails1 = (UserDetails) session.get(UserDetails.class,1);
-//        userDetails1.getListOfVehicle().stream()
-//                .forEach(s -> System.out.println(s.getVehicleName()));
-//        session.getTransaction().commit();
-//        session.close();
     }
 }
